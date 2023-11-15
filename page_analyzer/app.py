602b613aeee5c0f4e42d6199feac7f1cc4ccbd0a
deusmg <laptevap@gmail.com>
2023-11-15 23:54:09 +0300

    edit app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from dotenv import load_dotenv
 import psycopg2
-from psycopg2 import pool
 from urllib.parse import urlparse
-from validators.url import url
+from validators.url import url as validate_url
 from datetime import datetime
 
 app = Flask(__name__)
@@ -13,15 +12,27 @@ app.secret_key = "secret_key"
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
-connection_pool = psycopg2.pool.SimpleConnectionPool(
-    minconn=1,
-    maxconn=10,
-    dsn=DATABASE_URL
-)
+def is_valid_url(url):
+    return validate_url(url)
 
-def get_connection():
-    return connection_pool.getconn()
+def url_exists(cursor, url):
+    cursor.execute("SELECT COUNT(*) FROM urls WHERE name = %s", (url,))
+    count = cursor.fetchone()[0]
+    return count > 0
+
+def insert_url(cursor, url):
+    cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (url, datetime.today()))
+    conn.commit()
+
+def get_all_urls(cursor):
+    cursor.execute("SELECT * FROM urls")
+    return cursor.fetchall()
+
+def get_url_by_id(cursor, url_id):
+    cursor.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
+    return cursor.fetchone()
 
 @app.route('/')
 def index():
@@ -29,48 +40,32 @@ def index():
 
 @app.route('/add_url', methods=['POST'])
 def add_url():
-    url_input = request.form['url']
-    try:
-        conn = get_connection()
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT COUNT(*) FROM urls WHERE name = %s", (url_input,))
-            count = cursor.fetchone()[0]
-            if count == 0:
-                cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, NOW())", (url_input,))
-                conn.commit()
-                flash('URL добавлен успешно', 'success')
-            else:
-                flash('URL уже существует', 'error')
-    except psycopg2.Error as e:
-        app.logger.error(f"Error executing SQL: {e}")
-        flash('Произошла ошибка при выполнении запроса к базе данных', 'error')
-    finally:
-        conn.close()
+    url_input = request.form.get('url')
+
+    if not is_valid_url(url_input):
+        flash('Невалидный URL', 'error')
+        return redirect(url_for('index'))
+
+    with conn.cursor() as cursor:
+        if url_exists(cursor, url_input):
+            flash('Этот URL уже существует', 'error')
+        else:
+            insert_url(cursor, url_input)
+            flash('URL успешно добавлен', 'success')
 
     return redirect(url_for('index'))
-
+    
 @app.route('/urls')
 def show_urls():
-    try:
-        conn = get_connection()
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM urls")
-            urls = cursor.fetchall()
-    except psycopg2.Error as e:
-        app.logger.error(f"Error executing SQL: {e}")
-        flash('Произошла ошибка при выполнении запроса к базе данных', 'error')
-        return redirect(url_for('index'))
-    finally:
-        conn.close()
+    with conn.cursor() as cursor:
+        urls = get_all_urls(cursor)
 
     return render_template('urls.html', urls=urls)
 
 @app.route('/urls/<int:url_id>')
 def show_url(url_id):
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
-            url_data = cursor.fetchone()
+    with conn.cursor() as cursor:
+        url_data = get_url_by_id(cursor, url_id)
 
     if url_data:
         return render_template('url.html', url=url_data)
