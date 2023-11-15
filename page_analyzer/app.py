4eb6304a4b9580cdca6b8c45d4da761752d3766d
deusmg <laptevap@gmail.com>
2023-11-15 20:53:17 +0300

    add urls.html, url.html, base.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from dotenv import load_dotenv
 import psycopg2
 from urllib.parse import urlparse
 from validators.url import url
 
+
 app = Flask(__name__)
+
+app.secret_key = "secret_key"
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -20,13 +23,34 @@ def add_url():
     url_input = request.form.get('url')
     
     if not url(url_input):
-        return render_template('index.html', error='Невалидный URL')
+        flash('Невалидный URL', 'error')
+        return redirect(url_for('index'))
 
     with conn.cursor() as cursor:
         cursor.execute("INSERT INTO urls (name) VALUES (%s)", (url_input,))
         conn.commit()
 
+    flash('URL успешно добавлен', 'success')
     return redirect(url_for('index'))
+
+@app.route('/urls')
+def show_urls():
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM urls")
+        urls = cursor.fetchall()
+
+    return render_template('urls.html', urls=urls)
+
+@app.route('/urls/<int:url_id>')
+def show_url(url_id):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
+        url_data = cursor.fetchone()
+
+    if url_data:
+        return render_template('url.html', url=url_data)
+    else:
+        return redirect(url_for('show_urls'))
 
 if __name__ == '__main__':
     app.run(debug=True)
