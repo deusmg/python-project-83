5344a10df5c2e0335a6c245562335e408fe1189f
deusmg <laptevap@gmail.com>
2024-01-14 20:16:16 +0300

    edit func post_url_check

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -25,21 +25,23 @@ def prepare_url(input_url):
 def parse_html(html_str):
     page_content = BeautifulSoup(html_str, features="html.parser")
 
-    title = ''
-    h1 = ''
-    description = ''
+    result = {
+        'title': '',
+        'h1': '',
+        'description': ''
+    }
 
     title_element = page_content.title
     if title_element:
-        title = title_element.text
+        result['title'] = title_element.text
 
     h1_element = page_content.h1
     if h1_element:
-        h1 = h1_element.text
+        result['h1'] = h1_element.text
 
     page_meta = page_content.findAll('meta')
     for el in page_meta:
         if el.get('name') == 'description':
-            description = el.get('content')
+            result['description'] = el.get('content')
 
-    return title, h1, description
+    return result
